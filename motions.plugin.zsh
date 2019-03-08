1e5e0e017f06140e018feebe6a645f7b09f53e5d
xPMo <PMo_@vivaldi.net>
2019-03-08 10:53:20 -0600

    add text objects

---
+++ motions.plugin.zsh
@@ -0,0 +1,1 @@
+motions.zsh
---
+++ motions.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+fpath+="${0:h}"
+
+# More text objects
+autoload -Uz select-quoted select-bracketed
+zle -N select-quoted
+zle -N select-bracketed
+for m in vicmd viopp; do
+	for seq in {a,i}{\',\",\`}; do
+		bindkey -M "$m" "$seq" select-quoted
+	done
+	for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+		bindkey -M $m $c select-bracketed
+	done
+done
+
